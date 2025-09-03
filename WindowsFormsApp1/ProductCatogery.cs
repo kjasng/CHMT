93390a4d3d3fe03eb86c69b0ff6954c0e21f3f45
mckibidi <141372945+ZhongLi-2004@users.noreply.github.com>
2025-09-03 21:54:54 +0700

    Add files via upload

---
+++ WindowsFormsApp1/ProductCatogery.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class ProductCategory
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public ProductCategory() { }
+
+        public ProductCategory(int id, string name, string description)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Description = description;
+        }
+    }
+}
